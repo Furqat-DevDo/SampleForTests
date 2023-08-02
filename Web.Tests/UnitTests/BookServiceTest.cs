558592b018346f600985da82ab990a8940845515
Furqat <89734235+Furqat-DevDo@users.noreply.github.com>
2023-08-02 18:09:02 +0500

    Test

---
+++ Web.Tests/UnitTests/BookServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using System.Text.Json;
+using Web.Controllers;
+using Web.Models;
+using Web.Services;
+
+namespace Web.Tests.UnitTests;
+
+public class BookServiceTest
+{
+
+    [Fact]
+    public void Should_Return_NewUser_When_Parameters_OK()
+    {
+        var newUser = new UserCreateModel
+        {
+           FullName = "Test",
+           Email = "Test",
+           Age = 1,
+           PersonalData = JsonSerializer.SerializeToDocument( new{ Adrress= "MyAddress" , Passport = "MyPassport"}),
+        };
+
+        
+
+        
+    }
+}
